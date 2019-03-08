0a1a9f60e9b4442a6405fc46b67e1954bddf16d0
Abdul ahad <ahadsheikh1999@gmail.com>
2019-03-08 22:14:23 +0600

    Codeforces JSON file java signature

---
+++ CodeTime/app/src/main/java/com/example/codetime/contest_data/codeforces/Data.java
@@ -0,0 +1,11 @@
+package com.example.codetime.contest_data.codeforces;
+
+import java.util.List;
+
+public class Data {
+    public static boolean STATUS;
+
+//    public String toString() {
+//        return String.format("title:%s,id:%d,children:%s,groups:%s", title, id, children, groups);
+//    }
+}
---
+++ CodeTime/app/src/main/java/com/example/codetime/contest_data/codeforces/Groups.java
@@ -0,0 +1,24 @@
+package com.example.codetime.contest_data.codeforces;
+
+import java.util.ArrayList;
+public class Groups extends Data{
+    public int id;
+    public  ArrayList<String> name;
+    public ArrayList<String> type;
+    public ArrayList<String> phase;
+    public ArrayList<String> frozen;
+    public ArrayList<Integer>durationSeconds;
+    public ArrayList<Integer>startTimeSeconds;
+    public ArrayList<Integer> relativeTimeSeconds;
+
+    // public Groups(){
+    //     name = new ArrayList<>();
+    //     phase = new ArrayList<>();
+    //     type = new ArrayList<>();
+    //     frozen = new ArrayList<>();
+    //     durationSeconds = new ArrayList<>();
+    //     startTimeSeconds = new ArrayList<>();
+    //     relativeTimeSeconds = new ArrayList<>();
+    // }
+
+}

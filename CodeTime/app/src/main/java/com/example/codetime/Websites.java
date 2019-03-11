d8561d7920bf525d192e8f110cf6e9fa5682e01b
arif <arifjaman325@gmail.com>
2019-03-11 02:05:00 -0700

    websites commit

---
+++ CodeTime/app/src/main/java/com/example/codetime/Websites.java
@@ -0,0 +1,13 @@
+package com.example.codetime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Websites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_websites);
+    }
+}
